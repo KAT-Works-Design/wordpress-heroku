63a9ab52c9ed35cc3808b7731c88d763e27078d1
Kovács-Ajtai Tamás <kovacs.ajtai.tamas@gmail.com>
2019-09-14 21:19:33 +0300

    S3 plugin fix

--- config/plugins/wordpress/wordpress-s3uploads.php
+++ config/plugins/wordpress/wordpress-s3uploads.php
@@ -4,7 +4,7 @@
  * @url: https://github.com/humanmade/S3-Uploads
  */
 if (!empty(getenv('AWS_S3_URL'))) {
-    $env = sscanf(getenv('AWS_S3_URL'), 's3://%[^:]:%[^@]@s3-%[^.].amazonaws.com/%s');
+    $env = sscanf(getenv('AWS_S3_URL'), 's3://%[^:@s3-]:%[^:]:%[^.].amazonaws.com/%s');
 
     define('S3_UPLOADS_AUTOENABLE', true);
     define('S3_UPLOADS_KEY', $env[0]);
@@ -19,3 +19,4 @@ if (!empty(getenv('AWS_S3_URL'))) {
 
 // S3 Uploads - Cache will expire after 30 days
 define('S3_UPLOADS_HTTP_CACHE_CONTROL', 30 * 24 * 60 * 60);
+//s3://AKIAIUMCQ2ZMIZ4PLG2Q:ihjU4PrTobcvNZubaL82awfgV6FlaLO+1bnqP01t@s3.eu-west-3.amazonaws.com/creative-concept-solutions
